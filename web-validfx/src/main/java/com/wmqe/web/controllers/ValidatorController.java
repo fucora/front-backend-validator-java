bd92dbe5b3af63178d2ee4ff32a59bd409473fd4
march3 <gang.wei@qq.com>
2018-09-27 18:05:01 +0800

    commit

--- web-validfx/src/main/java/com/wmqe/web/controllers/ValidatorController.java
+++ web-validfx/src/main/java/com/wmqe/web/controllers/ValidatorController.java
@@ -1,8 +1,12 @@
 package com.wmqe.web.controllers;
 
 import com.alibaba.fastjson.JSONObject;
+import com.wmqe.web.validfx.annotations.Email;
+import com.wmqe.web.validfx.annotations.NotEmpty;
 import com.wmqe.web.validfx.models.ValidatorInfo;
+import com.wmqe.web.validfx.utils.Scanner;
 import com.wmqe.web.validfx.validators.EmailValidator;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,18 +47,91 @@ public class ValidatorController {
         return new ResponseEntity<Object>(ruleList, HttpStatus.OK);
     }
 
-    private List<ValidatorInfo> getValidatorInfos(String name){
-        List<ValidatorInfo> validatorInfoList = new ArrayList<>();
-        ValidatorInfo userId = new ValidatorInfo();
-        userId.setPropertyName("userId");
-        userId.setRequired(true);
-        ValidatorInfo email = new ValidatorInfo();
-        email.setPropertyName("email");
-        email.setRequired(true);
-        email.setRuleName(new String[]{"email"});
+    private Class<? extends ConstraintValidator<?,?>>[] getValidatedBy(Class<? extends Annotation> annotationType){
+        Constraint constraint = annotationType.getAnnotation(Constraint.class);
+        if(constraint == null)
+            return null;
 
-        validatorInfoList.add(userId);
-        validatorInfoList.add(email);
+        Class<? extends ConstraintValidator<?,?>>[] validatedBy = constraint.validatedBy();
+        if(validatedBy.length == 0)
+            return null;
+
+        return validatedBy;
+    }
+
+    private ValidatorInfo getValidatorInfo(Field field){
+        Annotation[] annotations = field.getAnnotations();
+
+        if(annotations.length == 0)
+            return null;
+
+        boolean required = false;
+        List<String> ruleList = new ArrayList<>();
+
+        for (int i = 0; i < annotations.length; i++) {
+            Annotation annotation = annotations[i];
+            Class annotationType = annotation.annotationType();
+            if(annotationType.isAssignableFrom(NotEmpty.class)){
+                required = true;
+                continue;
+            }
+            Class<? extends ConstraintValidator<?,?>>[] validatedBy = getValidatedBy(annotationType);
+            if(validatedBy != null){
+                String annotationName = annotationType.getSimpleName();
+                annotationName = annotationName.substring(0,1).toLowerCase() + annotationName.substring(1);
+                ruleList.add(annotationName);
+            }
+        }
+
+        if(ruleList.size() == 0)
+            return null;
+
+        String[] rules = new String[ruleList.size()];
+        ruleList.toArray(rules);
+
+        ValidatorInfo validatorInfo = new ValidatorInfo();
+        validatorInfo.setPropertyName(field.getName());
+        validatorInfo.setRequired(required);
+        validatorInfo.setRuleName(rules);
+
+        return validatorInfo;
+    }
+
+    private List<ValidatorInfo> getValidatorInfos(String name){
+
+        Class<?> cls = Scanner.getValidationModels(name);
+        if(cls == null)
+            return null;
+
+        Field[] fields = cls.getDeclaredFields();
+//        Method[] methods = cls.getMethods();
+//        for (int i = 0; i < methods.length ; i++) {
+//            Method method = methods[i];
+//
+//            //AnnotationUtils.findAnnotation()
+//
+//        }
+
+
+        List<ValidatorInfo> validatorInfoList = new ArrayList<>();
+        for (int i = 0; i < fields.length ; i++) {
+            Field field = fields[i];
+            ValidatorInfo vi = getValidatorInfo(field);
+            if(vi == null){
+                continue;
+            }
+            validatorInfoList.add(vi);
+        }
+//        ValidatorInfo userId = new ValidatorInfo();
+//        userId.setPropertyName("userId");
+//        userId.setRequired(true);
+//        ValidatorInfo email = new ValidatorInfo();
+//        email.setPropertyName("email");
+//        email.setRequired(true);
+//        email.setRuleName(new String[]{"email"});
+//
+//        validatorInfoList.add(userId);
+//        validatorInfoList.add(email);
         return validatorInfoList;
     }
 
--- web-validfx/src/main/java/com/wmqe/web/vo/UserVo.java
+++ web-validfx/src/main/java/com/wmqe/web/vo/UserVo.java
@@ -3,14 +3,16 @@ package com.wmqe.web.vo;
 import com.wmqe.web.validfx.annotations.Email;
 import com.wmqe.web.validfx.annotations.Length;
 import com.wmqe.web.validfx.annotations.NotEmpty;
+import com.wmqe.web.validfx.annotations.ValidationModel;
 
 import java.io.Serializable;
 
+@ValidationModel("user")
 public class UserVo implements Serializable {
     private static final long serialVersionUID = 9187584524512835680L;
 
     @NotEmpty
-    @Length(max=15)
+    @Length(max = 15)
     private String userId;
     @Email
     private String email;
