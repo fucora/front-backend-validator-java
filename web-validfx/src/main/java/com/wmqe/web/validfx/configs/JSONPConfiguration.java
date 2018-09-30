8b2963ade469f248ad00ed923b95e1b6f261af3a
march3 <gang.wei@qq.com>
2018-09-30 17:45:03 +0800

    commit

--- web-validfx/src/main/java/com/wmqe/web/validfx/configs/JSONPConfiguration.java
+++ web-validfx/src/main/java/com/wmqe/web/validfx/configs/JSONPConfiguration.java
@@ -2,11 +2,11 @@ package com.wmqe.web.validfx.configs;
 
 import com.wmqe.web.controllers.ValidatorController;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+//import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 
 @ControllerAdvice(basePackageClasses = {ValidatorController.class})
-public class JSONPConfiguration extends AbstractJsonpResponseBodyAdvice {
+public class JSONPConfiguration extends JsonpResponseBodyAdvice {
     public JSONPConfiguration(){
         super("callback","jsonp");
     }
